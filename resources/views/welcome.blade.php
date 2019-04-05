c984f35f99fce0320305e6c4aa7ad2c9acf2d270
zhongmairen <405213520@qq.com>
2019-04-05 02:36:45 +0000

    Initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello Laravel! - by 微能科技
+                    Hello Laravel! - by joyefly
                 </div>
 
                 <div class="links">
