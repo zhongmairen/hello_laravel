d5e8f4c15e5cf476ed690e9fc69cc6c695553183
zhongmairen <405213520@qq.com>
2019-04-06 11:38:12 +0000

    切割头部和尾部子视图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,24 +3,18 @@
 <head>
 <!--第一个参数是该区块的变量名称，第二个参数是默认值
  -->
-  <title>@yield('title', '微能') - 微能科技 - www.zwn.fun</title>
+  <title>@yield('title', '微能科技') - 为智慧商业赋能 - www.zwn.fun</title>
   <!-- <link rel="stylesheet" href="/css/app.css"> -->
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
+  @include('layouts._header')
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <a class="navbar-brand" href="/">微能科技</a>
-      <ul class="navbar-nav justify-content-end">
-          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
-          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
-           </ul>
-          </div>
-        </nav>
-
-        <div class="container">
-          @yield('content')
-        </div>
+  <div class="container">
+    <div class="offset-md-1 col-md-10">
+      @yield('content')
+      @include('layouts._footer')
+   </div>
+  </div>
 </body>
 </html>
