3c4858a6eac9f0d6ab5f45f08a39665c40e285a2
zhongmairen <405213520@qq.com>
2019-04-06 12:38:30 +0000

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 
 @section('content')
-   <div class="jumbotron">
+  <div class="jumbotron">
     <h1>Hello Laravel</h1>
     <p class="lead">
       你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
@@ -10,7 +10,7 @@
       一切，将从这里开始。
     </p>
     <p>
-      <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
     </p>
   </div>
 @stop
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/','StaticPagesController@home')->name('home');
-Route::get('/help','StaticPagesController@help')->name('help');
-Route::get('/about','StaticPagesController@about')->name('about');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+Route::get('signup', 'UsersController@create')->name('signup');
