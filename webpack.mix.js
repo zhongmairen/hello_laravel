34b8976101ccb71ad8c2c0322c171d0bc7e9517f
zhongmairen <405213520@qq.com>
2019-04-06 11:17:03 +0000

    静态文件浏览器缓存问题解决

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,6 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+//version()函数，只要文件修改，哈希值就会变，提醒客户端需要重新加载静态文件
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css').version();
