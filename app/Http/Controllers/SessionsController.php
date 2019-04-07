697271fdb5ae227bc9e7455b048f8896fc24e754
zhongmairen <405213520@qq.com>
2019-04-07 13:18:28 +0000

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -41,6 +41,7 @@ class SessionsController extends Controller
             Auth::logout();
                session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
                return redirect('/');
+           }
        } else {
                 // 登录失败后的相关操作
         session()->flash('danger', '很抱歉，您的邮箱和密码不匹配，请重新输入！');
