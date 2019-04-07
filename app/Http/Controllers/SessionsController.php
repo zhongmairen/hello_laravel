1ca9a40eab8af75f611ceaa97921acb7621bb177
zhongmairen <405213520@qq.com>
2019-04-07 08:36:42 +0000

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -22,7 +22,7 @@ class SessionsController extends Controller
        ]);
 
         #登录账密的数据库比对验证
-       if (Auth::attempt($credentials)) {
+       if (Auth::attempt($credentials,$request->has('remember'))) {
            // 登录成功后的相关操作
         session()->flash('success', '欢迎回来！');
         #Laravel 提供的 Auth::user() 方法来获取 当前登录用户 的信息，并将数据传送给路由。
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,3 +1,4 @@
+<!-- 用户登录视图-->
 @extends('layouts.default')
 @section('title', '登录')
 
@@ -21,6 +22,13 @@
           <div class="form-group">
             <label for="password">密码：</label>
             <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+          </div>
+          <!-- 登录视图，加上『记住我』复选框-->
+          <div class="form-group">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" name="remember" id="exampleCheck1">
+              <label class="form-check-label" for="exampleCheck1">记住我</label>
+            </div>
           </div>
 
           <button type="submit" class="btn btn-primary">登录</button>
